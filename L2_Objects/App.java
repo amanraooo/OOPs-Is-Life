7c25082a7d0d676683a83c4f2fa2d925cbc9fffa
amanraooo <ak9070777@gmail.com>
2026-01-12 23:07:00 +0530

    oops

---
+++ L2_Objects/App.java
@@ -0,0 +1,50 @@
+public class App{
+    public static void main(String[] args) {
+        // deafult constructor
+        Student A = new Student();
+        A.id = 1;
+        A.name= "Ayush";
+        A.nos=5;
+        A.age = 16;
+
+        System.out.println(A.id);
+        System.out.println(A.name);
+        System.out.println(A.nos);
+        System.out.println(A.age);
+
+        A.bunk();
+        A.study();
+        A.sleep();
+
+        System.out.println("-------");
+
+        //parameterised constructor
+        Student B = new Student(2, "Bablu", 15,5);
+
+        System.out.println(B.id);
+        System.out.println(B.name);
+        System.out.println(B.nos);
+        System.out.println(B.age);
+
+        B.bunk();
+        B.study();
+        B.sleep();
+
+        System.out.println("-------");
+
+        //copy constructor
+        Student C = new Student(B);
+
+        System.out.println(C.id);
+        System.out.println(C.name);
+        System.out.println(C.nos);
+        System.out.println(C.age);
+
+        C.bunk();
+        C.study();
+        C.sleep();
+
+        System.out.println("-------");
+
+    }
+}
---
+++ L2_Objects/Student.java
@@ -0,0 +1,50 @@
+public class Student {
+
+    // attributes
+    public int id ;
+    public String name;
+    public int age;
+    public int nos;
+
+
+    // default constructor
+    public Student(){
+        System.out.println("default constructor called");
+    }
+
+        //parameterised constructor
+        public Student(int id , String name, int age, int nos ){
+        System.out.println("parameterised constructor called");
+
+        this.id = id;
+        this.name= name;
+        this.age = age;
+        this.nos = nos;
+
+    }
+
+    // copy constructor
+    public Student(Student srcObj ){ // srcObj -> B
+        System.out.println("copy constructor called");
+
+        this.id = srcObj.id;
+        this.name= srcObj.name;
+        this.age = srcObj.age;
+        this.nos = srcObj.nos;
+
+    }
+    
+    // methods or behaviours
+    public void study(){
+        System.out.println(name+ " studying");
+    }
+
+    public void sleep(){
+        System.out.println(name+ " sleeping");
+    }
+
+    public void bunk(){
+        System.out.println(name+ " bunking");
+    }
+
+}
