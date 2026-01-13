fd1f5cd6ed2794277ee450e6dacc6e6a432e2fcb
amanraooo <ak9070777@gmail.com>
2026-01-13 13:31:07 +0530

    encapsulation

--- L3_EncapsulationAndAccessModifiers/App.java
+++ L3_EncapsulationAndAccessModifiers/App.java
@@ -1,8 +1,8 @@
 
 public class App {
     public static void main(String[] args) {
-        
-        Student A = new Student(1,"Aman", 22, 5);
+
+        Student A = new Student(1, "Aman", 22, 5);
         System.out.println(A.getName());
 
     }
