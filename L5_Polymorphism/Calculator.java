32cd2f3ce225a4ebe1397c4c7b37c8d8204b3513
amanraooo <ak9070777@gmail.com>
2026-01-15 15:40:42 +0530

    polymorphism

--- L5_Polymorphism/Calculator.java
+++ L5_Polymorphism/Calculator.java
@@ -1,5 +1,19 @@
 package L5_Polymorphism;
 
 public class Calculator {
-    
+
+    // method overloading : over argument count
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // method overloading : over argument type
+    double add(int a, int b, int c, double d) {
+        return a + b + c + d;
+    }
+
 }
