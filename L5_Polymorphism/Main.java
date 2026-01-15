aca8259c4cbf7d0a81227a3ae8e73de5ca456b68
amanraooo <ak9070777@gmail.com>
2026-01-15 15:43:40 +0530

    polymorphism

---
+++ L5_Polymorphism/Main.java
@@ -0,0 +1,13 @@
+package L5_Polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(2, 2));
+        System.out.println(c.add(2, 2, 2));
+        System.out.println(c.add(2, 2, 2,2.2));
+
+
+
+    }
+}
