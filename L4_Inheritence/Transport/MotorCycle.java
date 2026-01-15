a4ec9ff0cc24ccdbc55a234cc311b0e9c3def854
amanraooo <ak9070777@gmail.com>
2026-01-15 13:10:52 +0530

    inheritence

---
+++ L4_Inheritence/Transport/MotorCycle.java
@@ -0,0 +1,16 @@
+package L4_Inheritence.Transport;
+
+public class MotorCycle extends Vehicle {
+    public String handleBarStyle;
+    public String suspensionType;
+
+    MotorCycle(String name, String model, int noOfTyres, String handleBarStyle, String suspensionType){
+        super(name,model,noOfTyres);
+        this.handleBarStyle = handleBarStyle;
+        this.suspensionType = suspensionType;
+    }
+
+    public void wheelie(){
+        System.out.println("Motor cycle is doing whelieee "+ name);
+    }
+}
