968e1fca55031e26fd553b66ed26c3bb7151a264
amanraooo <ak9070777@gmail.com>
2026-01-15 21:59:21 +0530

    interface

---
+++ L6_Abstraction/InterfaceDesign/Main.java
@@ -0,0 +1,41 @@
+package L6_Abstraction.InterfaceDesign;
+
+interface Bird{
+    void fly();
+    void eat();
+}
+class Sparrow implements Bird {
+
+    @Override
+    public void eat() {
+        System.out.println("Sparrow eating");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Sparrow flying");
+    }
+}
+
+class Crow implements Bird {
+    @Override
+    public void eat() {
+        System.out.println("Crow eating");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Crow flying");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        doBirdStuff(new Sparrow());
+        doBirdStuff(new Crow());
+    }
+    public static void doBirdStuff(Bird b){
+        b.eat();
+        b.fly();
+    }
+}
