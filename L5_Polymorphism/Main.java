61234502d1403346dcad0270f1f0bcb562933b76
amanraooo <ak9070777@gmail.com>
2026-01-15 16:18:27 +0530

    polymorphism

--- L5_Polymorphism/Main.java
+++ L5_Polymorphism/Main.java
@@ -2,16 +2,33 @@ package L5_Polymorphism;
 
 public class Main {
     public static void main(String[] args) {
+
+        // compile time polymorphism : method overloading
         Calculator cal = new Calculator();
         System.out.println(cal.add(2, 2));
         System.out.println(cal.add(2, 2, 2));
         System.out.println(cal.add(2, 2, 2,2.2));
 
+        // run time polymorphism
+        // Circle cir = new Circle();
+        // cir.draw();
+
+        // Rectangle rect = new Rectangle();
+        // rect.draw();
+
+        // run time polymorphism : upcasting
         Circle cir = new Circle();
-        cir.draw();
+        doDrawinStuff(cir);
 
         Rectangle rect = new Rectangle();
-        rect.draw();
+        doDrawinStuff(rect);
 
+        Shape s = new Shape();
+        doDrawinStuff(s);    
+
+    }
+    //for dynamic method dispatch (upcasting)
+    public static void doDrawinStuff(Shape s){
+        s.draw();
     }
 }
