5d194d931c1b502a486d4e34254111432d0e1f62
amanraooo <ak9070777@gmail.com>
2026-01-20 14:48:48 +0530

    custom class

---
+++ L8_Java_Collection_Framework_2/Student.java
@@ -0,0 +1,36 @@
+package L8_Java_Collection_Framework_2;
+
+import java.util.Objects;
+
+public class Student {
+    public int rollNo;
+    public String name;
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Student student = (Student) o;
+        return rollNo == student.rollNo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rollNo);
+    }
+}
