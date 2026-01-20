9ba8fbef06a82c26a44a8032bbcb01c2e50cc5d3
amanraooo <ak9070777@gmail.com>
2026-01-20 14:47:31 +0530

    Hashset

--- L8_Java_Collection_Framework_2/HashSetBasics.java
+++ L8_Java_Collection_Framework_2/HashSetBasics.java
@@ -11,6 +11,18 @@ public class HashSetBasics {
         // LinkedHashedSet -> O(n)
         // TreeSet -> BST -> O(logn)
 
+        HashSet<Student> set = new HashSet<>();
+
+        Student s1 = new Student(1, "Babbar");
+        Student s2 = new Student(1, "Babbar");
+        Student s3 = new Student(1, "Babbar");
+
+        set.add(s1);
+        set.add(s2);
+        set.add(s3);
+
+        System.out.println(set);
+
         Set<Integer> st = new HashSet<>();
         st.add(40);
         st.add(10);
