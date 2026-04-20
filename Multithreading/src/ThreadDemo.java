c8501fb2d65f807b2dc3c26040ae43506ac92fab
Aman Gaade <ak9070777@gmail.com>
2026-04-20 16:47:27 +0530

    thread demo

--- Multithreading/src/ThreadDemo.java
+++ Multithreading/src/ThreadDemo.java
@@ -1,2 +1,19 @@
+class MyThread extends Thread{
+
+	@Override
+	public void run(){
+		for (int i = 0; i < 5; i++) {
+			System.out.println("child thread");
+		}
+	}
+}
+
 public class ThreadDemo {
+	public static void main(String[] args) {
+		MyThread t = new MyThread();
+		t.start();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("main");
+		}
+	}
 }
