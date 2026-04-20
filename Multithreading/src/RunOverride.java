926fd0fa6639ddf239cfa40dcd08ed62934628c1
Aman Gaade <ak9070777@gmail.com>
2026-04-20 16:51:26 +0530

    understanding run()

--- Multithreading/src/RunOverride.java
+++ Multithreading/src/RunOverride.java
@@ -1,2 +1,15 @@
+class MyRunThread extends Thread{
+
+	//without overriding run() no task will be cretaed no output given
+}
+
+
 public class RunOverride {
+	public static void main(String[] args) {
+		MyRunThread t = new MyRunThread();
+		t.start();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("main");
+		}
+	}
 }
