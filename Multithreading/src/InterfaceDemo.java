be52a7d69f2d4c797a88e50c26eedd1f2d4082ea
Aman Gaade <ak9070777@gmail.com>
2026-04-21 13:48:57 +0530

    InterfaceDemo

--- Multithreading/src/InterfaceDemo.java
+++ Multithreading/src/InterfaceDemo.java
@@ -1,2 +1,15 @@
-public class InterfaceDemo {
+
+class MyTask implements Runnable{
+
+	@Override
+	public void run(){
+		System.out.println("runnable....");
+	}
 }
+public class InterfaceDemo {
+	public static void main(String[] args) {
+		Thread t = new Thread(new MyTask());
+		t.start();
+	}
+}
+
