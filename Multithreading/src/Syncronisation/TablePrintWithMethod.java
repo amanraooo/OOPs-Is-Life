159049f9a089bfc65d74b21cd12c6cdaaaf9a2f5
Aman Gaade <ak9070777@gmail.com>
2026-04-22 07:45:04 +0530

    sync

---
+++ Multithreading/src/Syncronisation/TablePrintWithMethod.java
@@ -0,0 +1,16 @@
+package Syncronisation;
+
+public class TablePrintWithMethod {
+
+	public synchronized void printTable(int n){
+		for(int i =1; i<=5; i++){
+			System.out.println(Thread.currentThread().getName());
+			try{
+				Thread.sleep(500);
+
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
