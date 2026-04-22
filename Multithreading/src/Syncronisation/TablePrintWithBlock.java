0bb12709ed6917def992bed5c0c7c6ea3367bb52
Aman Gaade <ak9070777@gmail.com>
2026-04-22 08:19:24 +0530

    sync

--- Multithreading/src/Syncronisation/TablePrintWithBlock.java
+++ Multithreading/src/Syncronisation/TablePrintWithBlock.java
@@ -1,5 +1,17 @@
 package Syncronisation;
 
 public class TablePrintWithBlock {
+	public synchronized void printTable(int n){
+		synchronized (this){
+			for(int i =1; i<=5; i++){
+				System.out.println(Thread.currentThread().getName()+ " : "+(n*i));
+				try{
+					Thread.sleep(500);
 
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
