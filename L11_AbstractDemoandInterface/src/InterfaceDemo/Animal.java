6acd5b63dff4a71546e5213fc09312310a19d277
Aman Gaade <ak9070777@gmail.com>
2026-04-23 08:16:00 +0530

    interface

--- L11_AbstractDemoandInterface/src/InterfaceDemo/Animal.java
+++ L11_AbstractDemoandInterface/src/InterfaceDemo/Animal.java
@@ -1,4 +1,26 @@
 package InterfaceDemo;
 
 public interface Animal {
+
+	void makeSound();
+	void eats();
+}
+
+class Dog implements Animal{
+
+	@Override
+	public void makeSound(){
+		System.out.println("Dog barks");
+	}
+
+	@Override
+	public void eats(){
+		System.out.println("Dog eats roti");
+	}
+
+	public static void main(String[] args) {
+		Dog kaalu = new Dog();
+		kaalu.eats();
+		kaalu.makeSound();
+	}
 }
