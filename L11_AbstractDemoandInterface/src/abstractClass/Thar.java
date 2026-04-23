ccbfe3ab07c13b7ff7b1f97cce2bfb0e303a4aba
Aman Gaade <ak9070777@gmail.com>
2026-04-23 08:07:43 +0530

    abstract class

---
+++ L11_AbstractDemoandInterface/src/abstractClass/Thar.java
@@ -0,0 +1,19 @@
+package abstractClass;
+
+public class Thar extends Vehicle{
+	Thar(String brand) {
+		super(brand); //parent class constructor
+		//
+	}
+
+	@Override
+	void start() {
+
+	}
+
+	public static void main(String[] args) {
+		//Vehicle c=new Vehicle();
+
+	}
+
+}
