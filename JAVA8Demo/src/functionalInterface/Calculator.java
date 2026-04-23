f545a4352a2f70e540fc4ece675da115e71afeff
Aman Gaade <ak9070777@gmail.com>
2026-04-23 09:32:04 +0530

    functional interface

--- JAVA8Demo/src/functionalInterface/Calculator.java
+++ JAVA8Demo/src/functionalInterface/Calculator.java
@@ -1,4 +1,29 @@
 package functionalInterface;
 
+@FunctionalInterface
 public interface Calculator {
+
+	int add(int a, int b);
 }
+
+// we have no need to do this
+class child implements Calculator{
+
+	@Override
+	public int add(int a, int b){
+		return a+b;
+	}
+
+}
+
+//after java 8
+
+class Driver{
+	public static void main(String[] args) {
+		Calculator addition = (a,b)->a+b;
+		System.out.println("addition "+ addition.add(2,2));
+	}
+}
+
+
+
