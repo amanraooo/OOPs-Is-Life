4f014f0a19ac646e4f72e47ad061ab2a8ceeb854
Aman Gaade <ak9070777@gmail.com>
2026-04-27 08:42:49 +0530

    lock demo

--- Multithreading/src/LockDemo/BankAccount.java
+++ Multithreading/src/LockDemo/BankAccount.java
@@ -1,4 +1,30 @@
 package LockDemo;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
+	public int bal = 15000;
+	private final ReentrantLock rl = new ReentrantLock();
+
+	public void withdraw(int amount){
+		rl.lock();
+
+		try{
+			if(bal>amount){
+				Thread.sleep(2000);
+				bal=bal-amount;
+				System.out.println("withdrawn: "+ amount+ "remaining: "+ bal);
+			}
+			else {
+				System.out.println("insufficient balance");
+			}
+		}
+		catch(InterruptedException e){
+			throw new RuntimeException(e);
+		}
+		finally {
+			rl.unlock();
+		}
+
+	}
 }
