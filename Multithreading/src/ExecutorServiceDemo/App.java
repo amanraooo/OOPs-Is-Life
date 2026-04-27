8fe796bacd56afd546ce2052b54157ff2c58f680
Aman Gaade <ak9070777@gmail.com>
2026-04-27 10:01:15 +0530

    ExecutorServiceDemo

--- Multithreading/src/ExecutorServiceDemo/App.java
+++ Multithreading/src/ExecutorServiceDemo/App.java
@@ -7,8 +7,8 @@ public class App {
 	public static void main(String[] args) {
 		ExecutorService service= Executors.newFixedThreadPool(8);
 		String []studentList = {
-				"Riya","raj","Ashu","Aman","Sachin","Sunny",
-				"Ankit","Adarsh","rohit","Archana","Shashank","Sagar","Anju","Saurabh"
+				"Rohit","Yash","Arzoo","Aman","Ravi","Srishti",
+				"Kanika","Promila","Prem","Sajal","Sumit","Himanshu","Ranbir"
 		};
 
 		for(String student:studentList)
