81621682e26dd4c2df3d9e1175f891c5fecc3848
Aman Gaade <ak9070777@gmail.com>
2026-04-27 09:26:09 +0530

    lambda demo

--- Multithreading/src/LockDemo/Driver.java
+++ Multithreading/src/LockDemo/Driver.java
@@ -1,4 +1,14 @@
 package LockDemo;
 
 public class Driver {
+	public static void main(String[] args) {
+		BankAccount account = new BankAccount();
+
+		Runnable t1 = ()-> account.withdraw("maa",1000);
+		Runnable t2 = ()-> account.withdraw("me",157000);
+
+		new Thread(t1).start();
+		new Thread(t2).start();
+
+	}
 }
