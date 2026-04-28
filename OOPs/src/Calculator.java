6aad5eb335eb3b5f791cb93251d8c0fd1810c5eb
Aman Gaade <ak9070777@gmail.com>
2026-04-28 14:02:55 +0530

    oops revision

---
+++ OOPs/src/Calculator.java
@@ -0,0 +1,19 @@
+package OOPs.src;
+
+
+public class Calculator {
+	int add(int a,int b)
+	{
+		return a+b;
+	}
+
+	double add(double a, double b)
+	{
+		return a+b;
+	}
+
+	int add(int a, int b,int c)
+	{
+		return a+b;
+	}
+}
